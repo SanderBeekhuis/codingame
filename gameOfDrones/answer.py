05a6a3e26c8efb612052d9728307f5cd27cf90df
Sander Beekhuis <sander.beekhuis@live.nl>
2016-10-08 01:31:35 +0200

    [GoD] Added classes, try to move redundant drones

--- gameOfDrones/answer.py
+++ gameOfDrones/answer.py
@@ -1,10 +1,11 @@
-#FXME zone ownership
-#FXME make drone +zone class
-#Feature how many drones in a zone
+#TODO symmetry breaking/ release drones in losing situation
 
 import sys
 import math
 import collections
+
+#constants
+ZONERADIUS =100
 
 #init data structure
 zones = []
@@ -12,13 +13,23 @@ drones = []
 Pos = collections.namedtuple("Pos", ["x", "y"])
 
 class Drone:
-    def __init__(self):
-        self.pos, self.targetZone = None, None
+    def __init__(self, id):
+        self.pos, self._targetZone, self.id = None, None, id
 
-    def printMove(self):
+    def print_move(self):
         print(str(self.targetZone.pos.x) + " " + str(self.targetZone.pos.y))
 
-    def setTargetZoneToClosest(self):
+    def find_target(self):
+        sortedZones = sorted (zones, key=lambda z: z.priority())
+        if sortedZones[-1].priority() < -50:
+            self.set_target_zone_to_closest()
+            print("D{} setting target to CLOSEST zone {}".format(self.id, self.targetZone), file=sys.stderr)
+
+        else:
+            self.targetZone = sortedZones[-1]
+            print("D{} setting target PRIO zone {}".format(self.id, self.targetZone), file=sys.stderr)
+
+    def set_target_zone_to_closest(self):
         minDist = 10000
         target = -1
         for z in zones:
@@ -27,10 +38,78 @@ class Drone:
                 minDist = distance(self.pos, z.pos)
         self.targetZone = target
 
+    def __repr__(self):
+        return "D{}".format(self.id)
+
+    def _set_target_zone(self, target):
+        try:
+            self.targetZone.drones.remove(self)
+        except AttributeError:
+            pass #do nothing is previous target ws None
+        self._targetZone = target
+        target.drones.append(self)
+
+    def _get_target_zone(self):
+        return self._targetZone
+
+    targetZone = property(_get_target_zone, _set_target_zone)
 
 class Zone:
-    def __init__(self, pos):
-        self.pos, self.controller = pos, -1
+    def __init__(self,id, pos):
+        self.pos, self.id, self.controller, self.drones, self.playerDronesInZone = pos, id, -1, [], [0]*playerCount
+
+    def _max_enemies(self):
+        return max(self.playerDronesInZone[:playerId] + self.playerDronesInZone[playerId+1:])
+
+    def _future_drones(self):
+        return len(self.drones)
+
+    def priority(self):
+        #higer prio if number of enemies and our future drones are high
+        enCount = self._max_enemies()
+        futureCount = self._future_drones()
+        control = False
+        if self.controller == playerId:
+            control = True
+        if futureCount >= enCount and control:
+            return -100
+
+        if futureCount >= enCount + 1:
+            return -100
+
+        return - enCount + futureCount
+
+    def release_redundant_drones(self):
+        #init vars
+        enCount = self._max_enemies()
+        ourCount = self.playerDronesInZone[playerId]
+        futureCount = self._future_drones()
+
+        control = False
+        if self.controller == playerId:
+            control = True
+
+        #goalcount is the number of drones requierd to acquire and/or maintain control
+        if control:
+            goalCount = enCount
+        else:
+            goalCount = enCount +1
+
+
+        #delete redundant drones
+        self._release_drones(futureCount - goalCount)
+
+
+    def _release_drones(self, count):
+        if count>0:
+            print("Releasing {} drones from Z{}@({},{})".format(count, self.id, self.pos.x, self.pos.y), file= sys.stderr)
+            self.drones.sort( key = lambda d: distance(d.pos, self.pos))
+            for d in self.drones[-count:]:
+                d.find_target()
+
+
+    def __repr__(self):
+        return "Z{} Prio:{} Cont:{} Drone:{}".format(self.id, self.priority(), self.controller, self.drones)
 
 #define helper functions
 def distance(a, b):
@@ -42,26 +121,44 @@ def distance(a, b):
 # d: number of drones in each team (3 to 11)
 # z: number of zones on the map (4 to 8)
 playerCount, playerId, droneCount, zoneCount = [int(i) for i in input().split()]
-for _ in range(droneCount):
-    drones.append( Drone() )
-for _ in range(zoneCount):
+for id in range(zoneCount):
     # x: corresponds to the position of the center of a zone. A zone is a circle with a radius of 100 units.
     x, y = [int(j) for j in input().split()]
-    zones.append( Zone( Pos(x,y)))
+    zones.append( Zone(id=id,  pos=Pos(x,y)))
+for id in range(droneCount):
+    drones.append( Drone(id) )
 
 # game loop
 while True:
+    #update gamestate
     for z in zones:
-        z.controler = int(input())  # ID of the team controlling the zone (0, 1, 2, or 3) or -1 if it is not controlled. The zones are given in the same order as in the initialization.
+        test = int(input())
+        z.controller = test  # ID of the team controlling the zone (0, 1, 2, or 3) or -1 if it is not controlled. The zones are given in the same order as in the initialization.
+        z.playerDronesInZone = [0]*playerCount
     for i in range(playerCount):
         for d in drones:
-            # x: The first D lines contain the coordinates of drones of a player with the ID 0, the following D lines those of the drones of player 1, and thus it continues until the last player.
             x, y = [int(k) for k in input().split()]
+            pos = Pos(x,y)
+            #Add drone to area if it's in it
+            for z in zones:
+                if distance(pos, z.pos)<ZONERADIUS:
+                    z.playerDronesInZone[i]+=1
+            #update player drone positions
             if i == playerId:
-                d.pos = Pos(x,y)
+                d.pos = pos
 
+    #print status
+    for z in zones:
+        print(z, file = sys.stderr)
+
+    #make decisions
+    for z in zones:
+        z.release_redundant_drones()
+
+    #handle drones
     for d in drones:
+        #fallback targeting
         if d.targetZone == None:
-            d.setTargetZoneToClosest()
+            d.set_target_zone_to_closest()
 
-        d.printMove()
+        d.print_move()
