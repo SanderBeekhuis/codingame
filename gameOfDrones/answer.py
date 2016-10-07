f6ec82231f7bf9c2b8bd8cc74be7aff2ac073fc2
Sander Beekhuis <sander.beekhuis@live.nl>
2016-10-07 22:58:19 +0200

    Finished first bot
    Just goes to the closes zone and stays there.

--- gameOfDrones/answer.py
+++ gameOfDrones/answer.py
@@ -12,14 +12,20 @@ drones = []
 Pos = collections.namedtuple("Pos", ["x", "y"])
 
 class Drone:
-    def
-Drone = collections.namedtuple("Drone", ["pos", "target"])
-Zone = collections.namedtuple("Zone", ["pos", "controller"])
+    def __init__(self):
+        self.pos, self.targetZone = None, None
+
+    def printMove(self):
+        print(str(self.targetZone.pos.x) + " " + str(self.targetZone.pos.y))
+
+
+class Zone:
+    def __init__(self, pos):
+        self.pos, self.controller = pos, -1
 
 #define helper functions
 def distance(a, b):
     return math.sqrt((a.x -b.x) **2 + (a.y-b.y)**2)
-
 
 
 # p: number of players in the game (2 to 4 players)
@@ -28,16 +34,16 @@ def distance(a, b):
 # z: number of zones on the map (4 to 8)
 playerCount, playerId, droneCount, zoneCount = [int(i) for i in input().split()]
 for _ in range(droneCount):
-    drones.append( Drone(None, None) )
+    drones.append( Drone() )
 for _ in range(zoneCount):
     # x: corresponds to the position of the center of a zone. A zone is a circle with a radius of 100 units.
     x, y = [int(j) for j in input().split()]
-    zones.append( Zone( Pos(x,y),  -1))
+    zones.append( Zone( Pos(x,y)))
 
 # game loop
 while True:
-    for i in range(zoneCount):
-        tid = int(input())  # ID of the team controlling the zone (0, 1, 2, or 3) or -1 if it is not controlled. The zones are given in the same order as in the initialization.
+    for z in zones:
+        z.controler = int(input())  # ID of the team controlling the zone (0, 1, 2, or 3) or -1 if it is not controlled. The zones are given in the same order as in the initialization.
     for i in range(playerCount):
         for d in drones:
             # x: The first D lines contain the coordinates of drones of a player with the ID 0, the following D lines those of the drones of player 1, and thus it continues until the last player.
@@ -46,17 +52,13 @@ while True:
                 d.pos = Pos(x,y)
 
     for d in drones:
-        if d.target == None:
+        if d.targetZone == None:
             minDist = 10000
             target = -1
             for z in zones:
                 if distance(d.pos, z.pos) < minDist:
                     target = z
                     minDist = distance(d.pos, z.pos)
+            d.targetZone = target
 
-        # Write an action using print
-        # To debug: print("Debug messages...", file=sys.stderr)
-
-
-        # output a destination point to be reached by one of your drones. The first line corresponds to the first of your drones that you were provided as input, the next to the second, etc.
-        print(str(d.target.pos.x) + " " + str(d.target.pos.y))
+        d.printMove()
